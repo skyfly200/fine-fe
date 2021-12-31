11da5a3267015958726c5c2e0476d3a4d2656587
aldo <aldo.medina.z@gmail.com>
2021-12-31 18:42:01 +0000

    different env naming

--- client.ts
+++ client.ts
@@ -1,10 +1,7 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-  projectId:
-    process.env.NODE_ENV === 'development'
-      ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-      : process.env.SANITY_PROJECT_ID, // you can find this in sanity.json
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2021-08-31'
