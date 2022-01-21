78c188dae624700f27c7035653b2a881da6301a2
aldo <aldo.medina.z@gmail.com>
2022-01-21 16:35:52 +0000

    projects open api

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,3 +1,23 @@
-export default function handler(req, res) {
+import Cors from 'cors'
+import { projects } from '../../fixtures'
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD']
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors)
   res.status(200).json({ projects: projects })
 }
