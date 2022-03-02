c4e7bffdf197d370aa763385d03723f1eb652f4c
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-02 15:38:02 -0700

    refactor test

--- test/FineCore.test.js
+++ test/FineCore.test.js
@@ -2,18 +2,25 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("FineCore", function () {
-  it("Should return the FINE treasury address", async function () {
-    const FineCore = await ethers.getContractFactory("FineCore");
-    const core = await FineCore.deploy("0x7A832c86002323a5de3a317b3281Eb88EC3b2C00");
-    await core.deployed();
-
-    expect(await core.FINE_TREASURY()).to.equal("0x7A832c86002323a5de3a317b3281Eb88EC3b2C00");
-
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
+  before(async function () {
+    this.FineCore = await ethers.getContractFactory("FineCore");
   });
+
+  beforeEach(async function () {
+    this.core = await this.FineCore.deploy("0x7A832c86002323a5de3a317b3281Eb88EC3b2C00");
+    await this.core.deployed();
+  });
+
+  it("Should return the FINE treasury address", async function () {
+    expect(await this.core.FINE_TREASURY()).to.equal("0x7A832c86002323a5de3a317b3281Eb88EC3b2C00");
+  });
+
+  // it("Should return the FINE treasury address", async function () {
+  //   // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+  //   // // wait until the transaction is mined
+  //   // await setGreetingTx.wait();
+
+  //   // expect(await greeter.greet()).to.equal("Hola, mundo!");
+  // });
 });
