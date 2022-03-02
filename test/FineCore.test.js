62105e0eee2c8b510a496449f2d84cf16c874906
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-02 16:51:36 -0700

    test more of core

--- test/FineCore.test.js
+++ test/FineCore.test.js
@@ -15,12 +15,11 @@ describe("FineCore", function () {
     expect(await this.core.FINE_TREASURY()).to.equal("0x7A832c86002323a5de3a317b3281Eb88EC3b2C00");
   });
 
-  // it("Should return the FINE treasury address", async function () {
-  //   // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should return the platformPercentage", async function () {
+    expect(await this.core.platformPercentage()).to.equal("1000");
+  });
 
-  //   // // wait until the transaction is mined
-  //   // await setGreetingTx.wait();
-
-  //   // expect(await greeter.greet()).to.equal("Hola, mundo!");
-  // });
+  it("Should return the platformRoyalty", async function () {
+    expect(await this.core.platformRoyalty()).to.equal("3333");
+  });
 });
