ca94cd404895a0bc1f06c95d648ccdd6fac441e4
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-02 22:10:49 -0700

    fix off by 1 on premints complete check

--- contracts/FineShop.sol
+++ contracts/FineShop.sol
@@ -341,7 +341,7 @@ contract FineShop is AccessControl {
         require(projectLive[_projectId], "project not live");
         require(!projectPause[_projectId], "project paused");
         FineNFTInterface nftContract = FineNFTInterface(fineCore.getProjectAddress(_projectId));
-        require(nftContract.totalSupply() > projectPremintAllocation[_projectId], "premints remaining");
+        require(nftContract.totalSupply() >= projectPremintAllocation[_projectId], "premints remaining");
         require(count <= projectBulkMintCount[_projectId], "excedes minting limit");
         if (contractFilterProject[_projectId]) require(msg.sender == tx.origin, "No Contract Buys");
         if (projectMintLimit[_projectId] > 0) {
