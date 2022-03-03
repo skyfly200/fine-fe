80b75c40647e347aac16b7c937c5068a2403b473
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-03 01:47:01 -0700

    fix premint test

--- test/FineShop.test.js
+++ test/FineShop.test.js
@@ -89,17 +89,17 @@ describe("FineShop", function () {
     await this.shop.quickInit(0, owner.address, true, 0, 0);
     await this.shop.quickSet(0, "ETH", "0x0000000000000000000000000000000000000000", 10000, 1, 10);
     await this.shop.goLive(0);
-    await this.shop.premint(0); 
-    expect(await this.nft.totalSupply(0)).to.equal(1);
+    await this.shop.premint(0);
+    expect(await this.nft.totalSupply()).to.equal(1);
   });
 
-  it("Should be able to premint and allowlist mint", async function () {
+  it("Should be able to allowlist mint", async function () {
     await this.core.addProject(this.nft.address);
     const [owner] = await ethers.getSigners();
     await this.shop.quickInit(0, owner.address, true, 0, 0);
     await this.shop.quickSet(0, "ETH", "0x0000000000000000000000000000000000000000", 10000, 1, 10);
     await this.shop.goLive(0);
     await this.shop.premint(0);
-    expect(await this.nft.totalSupply(0)).to.equal(1);
+    expect(await this.nft.totalSupply()).to.equal(11);
   });
 });
