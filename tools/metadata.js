6754ae14957a0e5ce3744046399372e803d13955
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-07 17:09:02 -0700

    add json file save logic

--- tools/metadata.js
+++ tools/metadata.js
@@ -1,10 +1,23 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
+let folder = "json";
+
 fs.createReadStream('./solids.csv')
   .pipe(csv())
   .on('data', (row) => {
-    console.log(row);
+    // todo: transform json metadata
+
+    // convert JSON object to string
+    const data = JSON.stringify(row);
+
+    // write JSON string to a file
+    let filePath = './' + folder + '/' + row.No + '.json';
+    fs.writeFile(filePath, data, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
