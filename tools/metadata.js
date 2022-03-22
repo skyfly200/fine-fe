e21d97dbd230768e417d061d46a781784a50654e
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-21 20:16:24 -0600

    update metadata script

--- tools/metadata.js
+++ tools/metadata.js
@@ -3,17 +3,22 @@ const fs = require('fs');
 
 let folder = "json";
 
+let imagesBaseURI = "https://far.mypinata.cloud/ipfs/QmezRySqNs7CmtR1Cm1fBePP1QyFRUKc6eW2pP5fkFBLeM";
+let modelsBaseURI = "https://far.mypinata.cloud/ipfs/QmeWQYSZnZtRosgZpWrDL1ZeHXTkN6xtRJ2ykgJ7CSeNjT";
+
+let description = "";
+
 fs.createReadStream('./solids.csv')
   .pipe(csv())
   .on('data', (row) => {
     const metadata = {
-        description: "",
-        image: "" + row.No + "_SOLID01.png",
+        description: description,
+        image: imagesBaseURI + "/" + row.No + ".png",
         animation_url: "https://solids.fine.digital/preview/" + row.No,
         external_url: "https://solids.fine.digital/token/" + row.NO,
         name: "Solids",
         background_color: "#fff",
-        model_url: "" + row.No + ".fbx",
+        model_url: modelsBaseURI + "/" + row.No + ".glb",
         attributes: [
             {
                 trait_type: "Artwork ID",
