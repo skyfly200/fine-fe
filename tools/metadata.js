307a74ce97fda737b2e1b536fc739b5ff6fe9905
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-23 11:26:00 -0600

    build testnet metadata

--- tools/metadata.js
+++ tools/metadata.js
@@ -6,27 +6,27 @@ let folder = "json";
 let imagesBaseURI = "https://far.mypinata.cloud/ipfs/QmezRySqNs7CmtR1Cm1fBePP1QyFRUKc6eW2pP5fkFBLeM";
 let modelsBaseURI = "https://far.mypinata.cloud/ipfs/QmeWQYSZnZtRosgZpWrDL1ZeHXTkN6xtRJ2ykgJ7CSeNjT";
 
-let description = "";
+let description = "SOLIDS is a generative architecture project created by FAR. There will be 8,888 unique buildings generated algorithmically and compatible with Metaverses./n/nAs the Metaverse is becoming more ubiquitous, we are seeing many different examples of virtual worlds emerging. Immersive virtual worlds are not something novel; however, blockchain technology and cryptocurrency are bringing another level of excitement. The combination of technologies adds a more tangible layer to our virtual experience, which extends the material world./n/nIn this context of expansion into the Metasphere, Architecture is needed to shape the habitat of the Virtual Realm. This is where SOLIDS was born, as an essential response to the needs for buildings in the established and upcoming Digital Environments./n/nWhen the user mints a SOLID, they get a 3D file that can be imported into other 3D environments. Down the road, we will work on making them compatible with more Metaverses and environments as we evolve.";
 
 fs.createReadStream('./solids.csv')
   .pipe(csv())
   .on('data', (row) => {
     const metadata = {
         description: description,
-        image: imagesBaseURI + "/" + row.No + ".png",
-        animation_url: "https://solids.fine.digital/preview/" + row.No,
-        external_url: "https://solids.fine.digital/token/" + row.NO,
-        name: "Solids",
+        image: imagesBaseURI + "/" + row.ID + ".png",
+        animation_url: "https://solids-minisite-git-infura-aldomedinaz.vercel.app/preview/" + row.ID,
+        external_url: "https://solids-minisite-git-infura-aldomedinaz.vercel.app/",
+        name: "Solids #" + row.ID,
         background_color: "#fff",
-        model_url: modelsBaseURI + "/" + row.No + ".glb",
+        model_url: modelsBaseURI + "/" + row.ID + ".glb",
         attributes: [
             {
-                trait_type: "Artwork ID",
-                value: row.No,
+                trait_type: "Dimension",
+                value: row.Dim,
             },
             {
-                trait_type: "Dim",
-                value: row.Dim,
+                trait_type: "Size",
+                value: row.Size,
             },
             {
                 trait_type: "Skylight",
@@ -56,7 +56,7 @@ fs.createReadStream('./solids.csv')
 
 
     // write JSON string to a file
-    let filePath = './' + folder + '/' + row.No + '.json';
+    let filePath = './' + folder + '/' + row.ID + '.json';
     fs.writeFile(filePath, data, (err) => {
         if (err) {
             throw err;
