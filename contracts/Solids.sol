94753c1fc7d0f924f00b2f601a7077bbd2447d86
Skyler Fly-Wilson <skyfly200@users.noreply.github.com>
2022-03-30 10:04:57 -0600

    use IPFS protocol URIs in contract

--- contracts/Solids.sol
+++ contracts/Solids.sol
@@ -31,8 +31,8 @@ contract Solids is ERC721Enumerable, ERC721Burnable, ERC721Royalty, AccessContro
     uint256 public additionalPayeeRoyaltyPercentage = 0;
     uint96 public royaltyPercent = 4500;
 
-    string public _contractURI = "https://gateway.pinata.cloud/ipfs/Qmchn1kBhZv43jxPsPE4ijQuKxuSBuK2DyWJ8av5NXkQgt";
-    string public baseURI = "https://gateway.pinata.cloud/ipfs/QmeEky5aZNmog1fNdbsxAC5X1CkKCfrfns3tPqQ8rb1kiw";
+    string public _contractURI = "ipfs://Qmchn1kBhZv43jxPsPE4ijQuKxuSBuK2DyWJ8av5NXkQgt";
+    string public baseURI = "ipfs://QmQEJ389wv6hTotejokxqyURh3Ew1Ej2VfU4GRX9J37x7t";
     string public artist = "FAR";
     string public description = "a sample NFT for FINE";
     string public website = "https://fine.digital";
